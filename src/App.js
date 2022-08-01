bfb5f8d7cce0e25f71bd4e5d95241839b5b28b72
Musa Issa <musaissa@Musas-Air.broadband>
2022-08-01 15:24:57 +0100

    style nav

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
 		<div className="App">
 			<header className="App-header">
-				<Header />
+				{/* <Header /> */}
 			</header>
 < MainNav/>
 			<Routes  >
--- src/components/MainNav/MainNav.jsx
+++ src/components/MainNav/MainNav.jsx
@@ -1,6 +1,8 @@
-import {Link} from "react-router-dom";
-import {Container, Nav, Navbar} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import {Link} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 
 
@@ -8,15 +10,15 @@ export const MainNav = () => {
  
 	return (
 		<div>
-			<Navbar bg="dark" variant="dark">
-				<Container className="container">
-					<Navbar.Brand href="/" className="nav-font">
-						NC-GAMES
-					</Navbar.Brand>
-					<Nav >
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="/reviews">Reviews</Nav.Link>
-						<Nav.Link href="/users">Users</Nav.Link>
+			<Navbar>
+				<Container className="nav-links">
+					<Navbar.Brand className="nav-brand">NC-GAMES</Navbar.Brand>
+					<Nav>
+						<div>
+							<Nav.Link href="/">Home</Nav.Link>
+							<Nav.Link href="/reviews">Reviews</Nav.Link>
+							<Nav.Link href="/users">Users</Nav.Link>
+						</div>
 					</Nav>
 				</Container>
 			</Navbar>
