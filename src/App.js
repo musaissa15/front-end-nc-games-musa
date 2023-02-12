a93a9d6d7b226729a589adf99ff0db665d76bee1
Musa Imad Issa <musaissa004@gmail.com>
2023-02-12 00:01:42 +0000

    format code

--- src/App.js
+++ src/App.js
@@ -10,24 +10,25 @@ import "./components/Categories/CategoriesNav.css";
 import "./components/SingleReview/SingleReview.css";
 import "./components/Comments/Comments.css";
 import "./components/Users/Users.css";
-import "./components/Home/Home.css"
-import "./components/PostComments/PostComments.css"
-import "./components/MainNav/MainNav.css"
-import './components/SortReviews/SortReviews.css'
-import './components/DeleteComments/DeleteComments.css'
-import './components/SortReviews/OrderBy.css'
-import {MainNav} from "./components/MainNav/MainNav";
-import {useState} from 'react'
-import {UserContext} from './Context/UserContext'
+import "./components/Home/Home.css";
+import "./components/PostComments/PostComments.css";
+import "./components/MainNav/MainNav.css";
+import "./components/SortReviews/SortReviews.css";
+import "./components/DeleteComments/DeleteComments.css";
+import "./components/SortReviews/OrderBy.css";
+import { MainNav } from "./components/MainNav/MainNav";
+import { useState } from "react";
+import { UserContext } from "./Context/UserContext";
 
 function App() {
 	const [user, setUser] = useState({
-		
-		name: 'guest', username: 'guest', avatar_url:
-			"https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"
+		name: "guest",
+		username: "guest",
+		avatar_url:
+			"https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg",
 	});
-  return (
-		<UserContext.Provider value={{ user, setUser}}>
+	return (
+		<UserContext.Provider value={{ user, setUser }}>
 			<div className="App">
 				<header className="App-header"></header>
 				<MainNav />
